8eb660778551373040a0538cb865ed73be553d51
ccamilobedoya <ccamilo.bedoya@gmail.com>
2016-05-31 13:03:38 -0500

    Agregado codigo para peticion get en buscar vuelos

--- src/main/webapp/buscar_vuelos/buscar_vuelos.js
+++ src/main/webapp/buscar_vuelos/buscar_vuelos.js
@@ -12,11 +12,31 @@ angular.module('app.buscarvuelos', ['ngRoute'])
 
   .controller('buscarVuelosCtrl',
     function($scope, $http){
+      $scope.listaAeropuertos = ['BOG', 'OLH', 'JFK'];
 
+      $scope.desde;
+      $scope.hasta;
       $scope.datepicker =
         moment().format('YYYY-MM-DD') + "T" +
         moment().hours() + ":" +
         moment().minutes() + ":00Z";
+
+      $scope.vuelos = [];
+      $scope.buscarVuelos = function() {
+        var urlCompleta = 'http://localhost:8080/Aerolinea/rest/vuelos/busqueda' +
+          '?' +
+          'desde=' + $scope.desde + '&' +
+          'hasta=' + $scope.hasta + '&' +
+          'salida=' + $scope.datepicker;
+        $http({
+          method: 'GET',
+          url: urlCompleta
+        }).then(function successCallback(response) {
+          $scope.vuelos = response;
+        }, function errorCallback(response) {
+
+        });
+      };
 
       // Crea el date picker
       $('input[name="datepicker"]').daterangepicker({
